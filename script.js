e71cbaa7879c8907514aa853290c8c105aa2e8a2
SWATHY K <88014570+Swathy2412@users.noreply.github.com>
2021-08-18 21:22:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,17 +6,42 @@ $(document).ready(function() {
         else{
             $('.navbar').removeClass("sticky");
         }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
         
     });
-    // toggle menu/navebar script
+// slide-up script
+$('.scroll-up-btn').click(function(){
+    $('html').animate({scrollTop: 0});
+});
+// toggle menu/navebar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+    // typing animation
+    var typed = new Typed(".typing",{
+        strings: ["Student","Developer","Editor"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true,
+    });
+    var typed = new Typed(".typing-2",{
+        strings: ["Student","Developer","Editor"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true,
+    });
+
+
     //owl carousel
     $('.carousel').owlCarousel({
         margin: 20,
         loop: true,
+        autoplay: true,
         autoplayTimeOut: 2000,
         autoplayHoverPause:true,
         responsive: {
